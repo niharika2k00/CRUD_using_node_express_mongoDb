a568eaee84b4d675603c51c34ee3936e18222db4
niharika2k00 <dniharika16@gmail.com>
2022-11-05 21:29:53 +0530

    basic changes

--- server.js
+++ server.js
@@ -1,37 +1,23 @@
-
-
 // const express = require('express');
-
 
 // ECMASCRIPT MODULE
 // import colors from 'colors';
-import express from 'express';
-import dotenv from 'dotenv';
-import ConnectDB from './config/DB.js';
-import Choco_routes from './Routes/Choco_routes.js';
-
+import express from "express";
+import dotenv from "dotenv";
+import ConnectDB from "./config/DB.js";
+import Choco_routes from "./Routes/Choco_routes.js";
 
 dotenv.config();
 ConnectDB();
 const app = express();
-app.use(express.json());
-
-
+app.use(express.json()); //  It parses incoming JSON requests and puts the parsed data in req.
 
 console.log("Yeaahhh Running .........");
 
-
-// listen the connections on the specified host and port
-app.listen(3000, function () {
-    console.log('listening on backend server at port 4000');
-})
-
 // app.get(endpoint, callback)
-app.get('/', (req, res) => {
-    res.send('.get success !')
-})
-
-
+app.get("/", (req, res) => {
+  res.send(".get success !");
+});
 
 /* app.get('/fetchall', async () => {
     try {
@@ -47,15 +33,19 @@ app.get('/', (req, res) => {
 // *************************     Replacing previous way we can also write using app.use()     ******************************
 
 // SYNTAX: app.use(path, callback)
-app.use('/choco', Choco_routes);
+/* 
+         app.use() function/method used to mount the specified middleware function(s) at the given path.
+         Here, Callback function is MIDDLEWARE FUNCTION.
+*/
+app.use("/choco", Choco_routes);
 
+const port = process.env.PORT || 4000;
 
-// PORT = 4000 in the env
-const port = process.env.PORT || 4000
+// listen() function ---->  used to bind and listen the connections on the specified host and port
+app.listen(2000, function () {
+  console.log(`listening on backend server at port  ${port}`);
+});
 app.listen(port, console.log(`Server Connected in ${port} for ${process.env.NODE_ENV}`));
-
-
-
 
 /*
 
